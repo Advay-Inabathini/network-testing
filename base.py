515f0295ca8c5df1e2eebac12d2b2d7d78a5c014
advay@kali <iadvay@gmail.com>
2024-07-10 09:15:49 -0400

    correctly working ssh-test

--- base.py
+++ base.py
@@ -1,7 +1,19 @@
 import iperf3
 import pexpect
 
-iperf3test = pexpect.spawn('iperf3 -s')
-iperf3test.expect('listening')
-print(iperf3test.before.decode('utf-8'))
-print(iperf3test.after.decode('utf-8'))
+def ssh_connect(host, username, password):
+    print(f'connecting to {host} as {username}...')
+    child = pexpect.spawn(f'ssh {username}@{host}')
+    child.expect("password:")
+    child.sendline(password)
+    child.expect(pexpect.EOF)
+    print(child.before.decode('utf-8'))
+    return child
+
+server_ip = input("enter server device ip address: ")
+server_username = input("enter server device username: ")
+server_password = input("enter server device password: ")
+
+client_ip = input("enter client device ip address: ")
+client_username = input("enter client device username: ")
+client_password = input("enter client device password: ")
--- trials/ssh-test.py
+++ trials/ssh-test.py
@@ -5,11 +5,11 @@ def ssh_connect(host, username, password):
     child = pexpect.spawn(f'ssh {username}@{host}')
     child.expect("password:")
     child.sendline(password)
-    child.expect(pexpect.EOF)
+    child.expect([r'\$', r'#'])
     print(child.before.decode('utf-8'))
     return child
 
 chilled = ssh_connect('192.168.0.123', 'htpc', 'htpc')
 chilled.sendline('ls')
-chilled.expect(pexpect.EOF)
+chilled.expect([r'\$', r'#'])
 print(chilled.before.decode('utf-8'))
