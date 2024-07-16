8379ffce887b1a7d9562ae6e28f4239a12397594
advay@kali <iadvay@gmail.com>
2024-07-16 04:26:37 -0400

    changes to work with macbook machine

--- base.py
+++ base.py
@@ -4,9 +4,9 @@ import pexpect
 def ssh_connect(host, username, password):
     print(f'connecting to {host} as {username}...')
     child = pexpect.spawn(f'ssh {username}@{host}')
-    child.expect("password:")
+    child.expect("assword:")
     child.sendline(password)
-    child.expect([r'\$', r'#'])
+    child.expect([r'\$', r'#', r'%'])
     print(child.before.decode('utf-8'))
     return child
 
--- trials/ssh-test.py
+++ trials/ssh-test.py
@@ -3,13 +3,13 @@ import pexpect
 def ssh_connect(host, username, password):
     print(f'connecting to {host} as {username}...')
     child = pexpect.spawn(f'ssh {username}@{host}')
-    child.expect("password:")
+    child.expect("assword:")
     child.sendline(password)
-    child.expect([r'\$', r'#'])
+    child.expect([r'\$', r'#', r'%'])
     print(child.before.decode('utf-8'))
     return child
 
-chilled = ssh_connect('192.168.0.123', 'htpc', 'htpc')
+chilled = ssh_connect('ip', 'username', 'password')
 chilled.sendline('ls')
 chilled.expect([r'\$', r'#'])
 print(chilled.before.decode('utf-8'))
