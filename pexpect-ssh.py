b64ebdc09b6b6cde57e136fe3e523790774dcc26
advay@kali <iadvay@gmail.com>
2024-07-16 08:33:53 -0400

    cli mvp operational

--- pexpect-ssh.py
+++ pexpect-ssh.py
@@ -1,4 +1,3 @@
-import iperf3
 import pexpect
 
 def ssh_connect(host, username, password):
@@ -17,17 +16,23 @@ def send_command(process, command):
     print(process.before.decode('utf-8'))
 
 
-# server_ip = input("enter server device ip address: ")
-# server_username = input("enter server device username: ")
-# server_password = input("enter server device password: ")
+server_ip = input("enter server device ip address: ")
+server_username = input("enter server device username: ")
+server_password = input("enter server device password: ")
 
-# client_ip = input("enter client device ip address: ")
-# client_username = input("enter client device username: ")
-# client_password = input("enter client device password: ")
+client_ip = input("enter client device ip address: ")
+client_username = input("enter client device username: ")
+client_password = input("enter client device password: ")
 
+
+server_process = ssh_connect(server_ip, server_username, server_password)
+client_process = ssh_connect(client_ip, client_username, client_password)
 
 send_command(server_process, 'ls')
 send_command(client_process,'ls')
 
 send_command(server_process, 'echo server ready')
 send_command(client_process,'echo client ready')
+
+send_command(server_process, 'iperf3 -s -p 5500')
+send_command(client_process, f'iperf3 -c {server_ip} -p 5500')
